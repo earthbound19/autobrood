d30c67162475ba4ef77a25d43b0bc62a200d2e41
artdev@inboxalias.com <TPug7ZI*23ARaRYEcXD>
2016-07-05 22:55:33 -0600

    Further narrowed down setup needs in ./dependencies/instructions.
    Correction in render-flames-anim-fractorium.sh (and may need more debugging)

--- batch-scripts/render-flames-anim-fractorium.sh
+++ batch-scripts/render-flames-anim-fractorium.sh
@@ -2,7 +2,7 @@ render-flames-fractorium.sh
 
 cd render_output
 
-ffmpeg -y -r 29.97 -f image2 -i _alles_anim_fr-%07d.flam3.png -vcodec utvideo -r 29.97 _alles_anim.avi
+ffmpeg -y -r 29.97 -f image2 -i _alles_anim-%07d.flam3.png -vcodec utvideo -r 29.97 _alles_anim.avi
 	# render size options:
 	# -filter:v "crop=1280:720:x:y"
 	# -filter:v "crop=1920:1080:x:y"
