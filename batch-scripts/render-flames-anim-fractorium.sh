bac12061afab32317e83476bb0a7071538962fec
Alex Hall <artdev@inboxalias.com>
2016-07-07 21:34:54 -0600

    add one crop option

--- batch-scripts/render-flames-anim-fractorium.sh
+++ batch-scripts/render-flames-anim-fractorium.sh
@@ -9,6 +9,7 @@ ffmpeg -y -r 29.97 -f image2 -i _alles_anim-%07d.flam3.png -vcodec utvideo -r 29
 	# -filter:v "crop=1280:720:x:y"
 	# -filter:v "crop=1920:1080:x:y"
 # ffmpeg -y -i _alles_anim.avi -filter:v "crop=1920:1080:x:y" -crf 10 _alles_anim.mp4
+# ffmpeg -y -i _alles_anim.avi -filter:v "crop=1920:1080:x:y" -crf 10 _alles_anim.mp4
 ffmpeg -y -i _alles_anim.avi -filter:v "crop=640:350:x:y" -crf 14 _alles_anim.mp4
 # ffmpeg -y -i _alles_anim.avi -crf 13 _alles_anim.mp4
 # to rescale: -vf resize:<xpixels>:-1:x:y
