dc1d4bd46d8bc1c37942743e9f716da00c3f33d4
Alex Hall <artdev@inboxalias.com>
2016-07-26 18:43:40 -0600

    fetch random electric sheep from local generation 242 repository

---
+++ batch-scripts/fetchNgen242Genes.sh
@@ -0,0 +1,18 @@
+# GLOBALS:
+sheepDir=`cygpath -u "C:\autobrood\_resources\gen242"`
+sheepToFetch=13
+
+ls "C:\autobrood\_resources\gen242" > sheep.txt
+mapfile -t sheep < sheep.txt
+rm sheep.txt
+numSheep=${#sheep[@]}
+# get n random numbers in range of number of genome files; re http://stackoverflow.com/a/2567569/1397555
+seq 1 $numSheep | sort -R | head -n $sheepToFetch > gen242randomGenomes.txt
+
+mapfile -t randomSheepIndexes < gen242randomGenomes.txt
+rm gen242randomGenomes.txt
+
+for element in "${randomSheepIndexes[@]}"
+do
+	cp "$sheepDir"/"${sheep[$element]}" .
+done
