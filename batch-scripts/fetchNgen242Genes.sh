fde34d59573b461afdd3aa08fd3fb4f3252ace97
Alex Hall <artdev@inboxalias.com>
2016-07-29 18:55:01 -0600

    auto-create brood folder after current date, time and microsecond; collect random genomes therein.

--- batch-scripts/fetchNgen242Genes.sh
+++ batch-scripts/fetchNgen242Genes.sh
@@ -1,6 +1,6 @@
 # GLOBALS:
 sheepDir=`cygpath -u "C:\_resources\gen242"`
-sheepToFetch=13
+sheepToFetch=18
 
 ls $sheepDir > sheep.txt
 mapfile -t sheep < sheep.txt
@@ -12,7 +12,17 @@ seq 1 $numSheep | sort -R | head -n $sheepToFetch > gen242randomGenomes.txt
 mapfile -t randomSheepIndexes < gen242randomGenomes.txt
 rm gen242randomGenomes.txt
 
+# create directory named after current date, time and microsecond, to put our randomly collected sheep genomes in:
+timeStamp=`date +"%Y_%d_%m__%H_%M_%S__%N"`
+broodDir=brood_$timeStamp
+mkdir $broodDir
+cd $broodDir
+
 for element in "${randomSheepIndexes[@]}"
 do
 	cp "$sheepDir"/"${sheep[$element]}" .
 done
+
+cd ..
+# Optional; comment out the above line if used:
+# render-flames-fractorium.sh foo
