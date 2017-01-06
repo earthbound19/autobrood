5c7d6c5e263c891cf9e0ec02a9904263c7fb469c
earthbound19 <artdev@inboxalias.com>
2017-01-05 17:44:34 -0700

    tweak dependency setup script for library source problems

--- _setup/_installDependencies.sh
+++ _setup/_installDependencies.sh
@@ -6,7 +6,7 @@
 # re: http://superuser.com/a/941135
 # ALSO, mysteriously, installing perl xml_twig via cygwin may solve any errors after this script successfully runs all commands. 
 
-echo This script will attempt to install dependencies needed for the .sh scripts in autobrood to work under Cygwin.
+echo This script will attempt to install dependencies needed for the \.sh scripts in autobrood to work under Cygwin. Before running this script\, you must install wget, perl and perl-libxml-perl in Cygwin. Also\, if you see \"connection refused\" errors\, try running the Cygwin installer again\, selecting a different mirror\, and reinstalling wget to make that mirror \"stick\" \(I don\'t even know if reinstalling anything is necessary\, though\)\.
 echo "!============================================================"
 read -p "DO YOU WISH TO CONTINUE running this script? : y/n" CONDITION;
 if [ "$CONDITION" == "y" ]; then
@@ -24,8 +24,9 @@ if [ "$CONDITION" == "y" ]; then
 			apt-cyg install chere
 			chere -i -t mintty
 		apt-cyg install make
-		apt-cyg install perl
-		apt-cyg install perl-libxml-perl
+# IF YOU comment out the next two lines, make sure to install these modules via the Cygwin installer! :
+		# apt-cyg install perl
+		# apt-cyg install perl-libxml-perl
 		# wget --no-check-certificate http://www.xmltwig.org/xmltwig/XML-Twig-3.50.tar.gz
 		wget http://www.xmltwig.org/xmltwig/XML-Twig-3.50.tar.gz
 		tar xz < XML-Twig-3.50.tar.gz
