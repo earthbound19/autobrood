293e55e88b0e8f55b758322c50773beb45f59057
Alex Hall <ansible1@earthbound.io>
2018-12-13 22:53:43 -0700

    Rip genomes out of renders saved by Electric Sheep screensaver!

---
+++ scripts/extract_genomes_from_ES_renders.sh
@@ -0,0 +1,21 @@
+# DESCRIPTION
+# Extracts all flam3_genome tags (genome information) from all .png renders in a directory as saved via the "Keep rendered frames" Electric Sheep settings, saving them to .flame files named after the source render image. .flame files are saved alongside the original images.
+
+# USAGE
+# ./thisScript.sh
+
+
+# CODE
+# NOTES: The flam3_genome tag name was found (in a .png image render saved from the running electric sheep screensaver) via:
+# exiftool -s -flam3_genome sheep_247_32990_478.png > hyarf.txt
+
+# Create an array of all png file names in the current directory:
+array=(`gfind . -maxdepth 1 -iname \*.png -printf '%f\n' | sort`)
+for element in ${array[@]}			# iterate over all items in array
+do
+	fileNameNoExt=${element%.*}
+	echo "Extracting (hopefully) genome from source file $element and saving to $fileNameNoExt.flame . . ."
+	exiftool -b -flam3_genome $element > $fileNameNoExt.flame
+done
+
+echo DONE.
