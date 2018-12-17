e4f51ee5b0ade79bf56ee33a67f71cfb0fd8f2d8
Alex Hall <ansible1@earthbound.io>
2018-12-17 15:11:22 -0700

    upd. to current paths

--- set-env.bat
+++ set-env.bat
@@ -1,10 +1,10 @@
+ECHO This environment set is tweaked to match changes in this toolset but it is untested. Good luck . . .
 color 2A
 SET PATH=%PATH%;%CD%
 cd batch-scripts
 SET PATH=%PATH%;%CD%
-cd ..
-cd bin
+cd ..\bin\fractorium-win
 SET PATH=%PATH%;%CD%
-cd fractorium_openCL_GPU_fractal_flames
+cd ..\flam3-win
 SET PATH=%PATH%;%CD%
 cd ..\..
